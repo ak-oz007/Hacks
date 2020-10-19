3c3cebca73ad5d554bbc78cafdf25168deac7b8d
ak-oz007 <73106513+ak-oz007@users.noreply.github.com>
2020-10-19 15:36:32 +0530

    passing values

--- project_1.java
+++ project_1.java
@@ -30,7 +30,14 @@ public class Road_Toll {
 public class RoadToll_Main {
 
 	public static void main(String[] args) {
+		Road_Toll rt1 = new Road_Toll();
 		
+		rt1.type = "Sadan";
+		rt1.tiresCount = 4;
+		rt1.calculateToll();
+		
+		Road_Toll rt2 = new Road_Toll("Truck", 6);
+		rt2.calculateToll();
 		
 		
 		}
